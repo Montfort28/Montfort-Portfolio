1680c227676a29d89ebd4281035e8497674f07c9
Montfort <mugishamontfort28@gmail.com>
2025-11-30 23:24:35 +0200

    other small changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { personalInfo } from '../utils/data';
 
 const Hero = () => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      setMousePosition({ x: e.clientX, y: e.clientY });
-    };
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => window.removeEventListener('mousemove', handleMouseMove);
-  }, []);
-
   const techStack = [
     { name: "React", imagePath: "/images/tech/react.png", color: "#61dafb" },
     { name: "Next.js", imagePath: "/images/tech/nextjs.png", color: "#000000" },
